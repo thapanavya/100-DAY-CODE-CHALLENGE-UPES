259d585d2637e479a3753e56a83bc6d97e13ac1b
Navya Thapa <navyathapa2007@gmail.com>
2025-09-26 22:42:29 +0530

    Add files via upload

---
+++ 16-DAY16/Q31.c
@@ -0,0 +1,26 @@
+//Write a program to take a number as input and print its equivalent binary representation.
+
+#include <stdio.h>
+int main() {
+    int num;
+    int binary[32]; 
+    int i = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num == 0) {
+        printf("Binary: 0\n");
+        return 0;
+    }
+    while (num > 0) {
+        binary[i] = num % 2; 
+        num = num / 2;
+        i++;
+    }
+    printf("Binary: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+    printf("\n");
+    return 0;
+}
+
