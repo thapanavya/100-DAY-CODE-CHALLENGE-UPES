fa5cfc2050e999b9179ac2f8fc4e8fb88e9160c9
Navya Thapa <navyathapa2007@gmail.com>
2025-11-28 23:01:27 +0530

    Add files via upload

---
+++ 45-DAY45/Q89.c
@@ -0,0 +1,15 @@
+//Count frequency of a given character in a string.
+#include <stdio.h>
+int main() {
+    char str[100], ch;
+    int i, count=0;
+    gets(str);
+    printf("Enter character: ");
+    scanf("%c", &ch);
+
+    for(i=0; str[i]!='\0'; i++)
+        if(str[i]==ch) count++;
+
+    printf("Frequency = %d", count);
+}
+
---
+++ 45-DAY45/Q90.c
@@ -0,0 +1,14 @@
+//Toggle case of each character in a string.
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i;
+    gets(str);
+
+    for(i=0; str[i]!='\0'; i++){
+        if(str[i]>='a'&&str[i]<='z') str[i]-=32;
+        else if(str[i]>='A'&&str[i]<='Z') str[i]+=32;
+    }
+    printf("Toggled: %s", str);
+}
+
