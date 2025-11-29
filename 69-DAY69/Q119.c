c7208721c51475d346b205f173051cf9ecf1e2d7
Navya Thapa <navyathapa2007@gmail.com>
2025-11-29 23:55:47 +0530

    Add files via upload

---
+++ 69-DAY69/Q119.c
@@ -0,0 +1,19 @@
+//Write a program to take an integer array as input. Only one element will be repeated. Print the repeated element. Try to find the result in one single iteration.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n], freq[100000] = {0};  // adjust if needed
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        freq[arr[i]]++;
+        if(freq[arr[i]] > 1) {
+            printf("%d", arr[i]);
+            return 0;
+        }
+    }
+    return 0;
+}
+
