15f64652b34eabad1d63e5d5a5ac0cd13a29f759
Navya Thapa <navyathapa2007@gmail.com>
2025-11-29 23:34:58 +0530

    Add files via upload

---
+++ 52-DAY52/Q102.c
@@ -0,0 +1,26 @@
+//Write a Program to take a sorted array arr[] and an integer x as input, find the index (0-based) of the smallest element in arr[] that is greater than or equal to x and print it. This element is called the ceil of x. If such an element does not exist, print -1. Note: In case of multiple occurrences of ceil of x, return the index of the first occurrence.
+#include <stdio.h>
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+    int arr[n];
+    
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &x);
+
+    int index = -1;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] >= x) {
+            index = i;
+            break;
+        }
+    }
+
+    printf("%d", index);
+    return 0;
+}
+
+
