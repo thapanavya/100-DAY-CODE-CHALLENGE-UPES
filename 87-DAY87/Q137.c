1768f78eb6665e4906ab16671f8ccc42e276526f
Navya Thapa <navyathapa2007@gmail.com>
2025-11-30 21:45:11 +0530

    Add files via upload

---
+++ 87-DAY87/Q137.c
@@ -0,0 +1,15 @@
+//Create an enum for user roles (ADMIN, USER, GUEST) and display messages based on role.
+#include <stdio.h>
+
+enum Role { ADMIN, USER, GUEST };
+
+int main() {
+    enum Role r = USER;
+
+    if(r == ADMIN) printf("Full Access Granted\n");
+    else if(r == USER) printf("Limited Access\n");
+    else printf("Guest Access Only\n");
+
+    return 0;
+}
+
